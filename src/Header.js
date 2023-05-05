19dff1e286738bf6458e905fac67a3bf0814020f
Araf <arafsoloman3734@gmail.com>
2023-05-06 04:47:51 +0600

    Gmail project header and sidebar done

---
+++ src/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Header.css';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Avatar, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+function Header() {
+    return (
+        <div className='header'>
+            <div className='header_left'>
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+                <img src='https://cdn.vox-cdn.com/thumbor/jJ_w_lWMMvGKoaLp_zaEXJpyZ9c=/0x0:1320x880/1400x788/filters:focal(660x440:661x441)/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg' />
+            </div>
+            <div className='header_middle'>
+                <SearchIcon />
+                <input type='text' placeholder='Search mail' />
+                <ArrowDropDownIcon className='header_inputCaret' />
+            </div>
+            <div className='header_right'>
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+                <Avatar />
+            </div>
+        </div>
+    );
+}
+
+export default Header;
